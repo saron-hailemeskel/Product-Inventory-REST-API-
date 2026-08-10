7b2a08a7ce6ef98082a202e10aa19c7fce729d68
Saron Hailemeskel <saronhailemeskel15@gmail.com>
2026-08-10 21:52:17 +0300

    last commit

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -24,6 +24,6 @@ router.post("/",productController.createProduct);
 
 router.put("/:id", productController.updateProduct);
 
-router.delete("id", productController.deleteProduct);
+router.delete("/:id", productController.deleteProduct);
 
 module.exports = router;
